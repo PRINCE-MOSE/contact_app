79cbcc27957f72fe0b03896e739baaa382b11ff4
Moses S. Wekesa <MWekesa@helb.co.ke>
2022-07-25 15:33:16 +0300

    Initial Commit

---
+++ resources/views/contacts/create.blade.php
@@ -0,0 +1,2 @@
+<h1>Add a new contact</h1>
+<a href="{{route('contacts.index')}}">Back to all Contacts</a>
---
+++ resources/views/contacts/show.blade.php
@@ -0,0 +1,4 @@
+<h1>Contact Details</h1>
+<a href="{{route('contacts.index')}}">Back to all Contacts</a>
+
+<div> {{$contact}} </div>
